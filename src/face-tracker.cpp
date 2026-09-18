71a14ae45bee5aea28a458b07b6b7af7376c9419
alexvcs <a.krois@live.de>
2026-09-18 19:12:26 +0200

    Add landmark smoothing feature to face tracker

--- src/face-tracker.cpp
+++ src/face-tracker.cpp
@@ -20,9 +20,14 @@ static inline std::shared_ptr<texture_object> surface_to_cvtex(struct face_track
 class ft_manager_for_ftf : public face_tracker_manager {
 public:
 	struct face_tracker_filter *ctx;
+	float landmark_smoothing;
 
 public:
-	ft_manager_for_ftf(struct face_tracker_filter *ctx_) { ctx = ctx_; }
+	ft_manager_for_ftf(struct face_tracker_filter *ctx_)
+	{
+		ctx = ctx_;
+		landmark_smoothing = 0.80f;
+	}
 
 	~ft_manager_for_ftf() { release_cvtex(); }
 
@@ -89,6 +94,8 @@ static void ftf_update(void *data, obs_data_t *settings)
 	s->debug_faces = obs_data_get_bool(settings, "debug_faces");
 	s->debug_notrack = obs_data_get_bool(settings, "debug_notrack");
 	s->debug_always_show = obs_data_get_bool(settings, "debug_always_show");
+	s->ftm->landmark_smoothing =
+		(float)obs_data_get_double(settings, "landmark_smoothing") * 0.01f;
 
 	debug_data_open(&s->debug_data_tracker, &s->debug_data_tracker_last, settings, "debug_data_tracker");
 	debug_data_open(&s->debug_data_error, &s->debug_data_error_last, settings, "debug_data_error");
@@ -282,6 +289,9 @@ static obs_properties_t *ftf_properties(void *data)
 		obs_properties_add_bool(pp, "debug_faces", "Show face detection results");
 		obs_properties_add_bool(pp, "debug_notrack", "Stop tracking faces");
 		obs_properties_add_bool(pp, "debug_always_show", "Always show information (useful for demo)");
+		obs_property_t *landmark_smoothing = obs_properties_add_float_slider(
+			pp, "landmark_smoothing", "Landmark smoothing", 0.0, 95.0, 1.0);
+		obs_property_float_set_suffix(landmark_smoothing, " %");
 #ifdef ENABLE_DEBUG_DATA
 		obs_properties_add_path(pp, "debug_data_tracker", "Save correlation tracker data to file",
 					OBS_PATH_FILE_SAVE, DEBUG_DATA_PATH_FILTER, NULL);
@@ -316,6 +326,7 @@ static void ftf_get_defaults(obs_data_t *settings)
 {
 	obs_data_set_default_bool(settings, "preset_mask_track", true);
 	obs_data_set_default_bool(settings, "preset_mask_control", true);
+	obs_data_set_default_double(settings, "landmark_smoothing", 80.0);
 	face_tracker_manager::get_defaults(settings);
 	obs_data_set_default_double(settings, "track_z", 0.70);  //  1.00  0.50  0.35
 	obs_data_set_default_double(settings, "track_y", +0.00); // +0.00 +0.10 +0.30
